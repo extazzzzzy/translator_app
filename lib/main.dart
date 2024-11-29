ff669ec3dcf117cae71d36941814497934e83f7d
17Vector <131187562+17Vector@users.noreply.github.com>
2024-11-29 19:03:26 +0500

    Экран игры

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:translator/HomeScreen.dart';
+import 'package:translator/GamingScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: GameScreen(),
     );
   }
 }
